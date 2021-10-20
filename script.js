1823e36a95359ab75c284d7ca4770ae892e84d83
Nick Gertler <nickgertler@gmail.com>
2021-10-20 12:27:32 -0400

    Update noun generator to remove vowels.

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ const companyTLDs = messageComponents.companyTLDs;
 const businessTypes = messageComponents.businessTypes;
 const businessAreas = messageComponents.businessAreas;
 
-// Generate a noun to use as base of business name
+// Function to remove vowels from noun. Used to stylize company names.
+const removeVowels = string => {
+    return string.replace(/[aeiou]/g, '');
+};
+
+// Generate a noun to use as base of business name. 1/3 times will return stylized name with vowels removed.
 const genBusinessNoun = () => {
     const randWordIndex = Math.floor(Math.random() * companyWords.length);
-    return companyWords[randWordIndex];
+    const randWord = companyWords[randWordIndex];
+    
+    // Random has 3 possible outcomes. Conditional run 33% of the time. 
+    if (Math.floor(Math.random() * 3) == 2) {
+        return removeVowels(randWord)
+    } else {
+        return randWord
+    }
 };
