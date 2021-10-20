099865581eb7f07e947acca8109823e1e0781bfb
Nick Gertler <nickgertler@gmail.com>
2021-10-20 12:08:26 -0400

    Function to get random noun for business name

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const companyWords = messageComponents.companyWords;
 const companyTLDs = messageComponents.companyTLDs;
 const businessTypes = messageComponents.businessTypes;
 const businessAreas = messageComponents.businessAreas;
+
+// Generate a noun to use as base of business name
+const genBusinessNoun = () => {
+    const randWordIndex = Math.floor(Math.random() * companyWords.length);
+    return companyWords[randWordIndex];
+};
